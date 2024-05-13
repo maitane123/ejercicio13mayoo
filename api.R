a54b400f6bd2160b5a1000b18812cada707f256a
Maitane Castaño <maitane.castano@alumni.edu>
2024-05-13 12:39:48 +0200

    añado api a la rama

---
+++ api.R
@@ -0,0 +1,14 @@
+#* @appititle examen api 
+#* @apiDescription swager de plumber que tendre que hacer para agrupar examen 
+#* @param en resistencia minima requerdida 
+#* @param st fuerza minima requerdida
+#* @param Po potencia minima requerdida 
+#* @param st velocidad minima requerdida
+#* @param en agilidad minima requerdida 
+#* @param ft flexibilidad minima requerdida
+#* @param st flexibilidad minima requerdida
+
+
+
+
+depo
