ca8382a2f0a588bdafdc9b7085dc0d42658276ab
Maitane Castaño <maitane.castano@alumni.edu>
2024-05-13 12:27:50 +0200

    primera funcion

--- 13mayo.R
+++ 13mayo.R
@@ -33,4 +33,18 @@ length(unique(df$SPORT))
 
 ####dime que deportes necesitan mas de 8 en resietrencia menos de-6 en fuerza 
 df%>% filter(Endurance>8,Strength <6, Flexibility<5)%>%
-  select 
+  select(SPORT)
+
+#hacer una funcion que nos sirva para consultar que deporte tiene que caracteristicas
+
+consulta_deporte<-function(df,en = 0,st=0,po=0,sp=o,ag=0,
+                           ft=0,ne=0,du=0,ha=0,an=0){
+  deportes<-df%>%filter(Endourance>en,Strength>st,
+                        Power>po,Speed>sp,Agility>ag,Flexibility>ft,Nerve=ne,Durability>du,
+                        Hand.eye.coordination>ha,Analytical.Aptitude>an)%>%
+    select(SPORT)
+  return(deportes)
+}
+
+
+consulta_deporte(en=8)
